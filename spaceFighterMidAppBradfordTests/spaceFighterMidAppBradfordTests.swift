2ebb1c8fbbad88ec52dd914e2ea71e62748b19ae
cameron bradford <kylocam0712@gmail.com>
2025-02-26 07:48:13 -0600

    Initial Commit

---
+++ spaceFighterMidAppBradfordTests/spaceFighterMidAppBradfordTests.swift
@@ -0,0 +1,17 @@
+//
+//  spaceFighterMidAppBradfordTests.swift
+//  spaceFighterMidAppBradfordTests
+//
+//  Created by CAMERON BRADFORD on 2/26/25.
+//
+
+import Testing
+@testable import spaceFighterMidAppBradford
+
+struct spaceFighterMidAppBradfordTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
